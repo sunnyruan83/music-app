49b95aa9e2090bd390b64c5e131ab933672b3388
sunnyruan83 <sunnyruan83@gmail.com>
2019-05-07 17:27:43 +0100

    final

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import logo from './logo.jpg';
+import React from "react";
+import logo from "./logo.jpg";
+import Index from "./index.css";
 
 const Header = () => {
   return (
     <nav className="header">
-     <img
-        src={logo}
-        alt="LOGO"
-        style={{ width: '600px' }}
-      />
+      <img src={logo} alt="LOGO" style={{ width: "600px" }} />
+      <div className="musicwave">
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+        <span />
+      </div>
     </nav>
   );
 };
