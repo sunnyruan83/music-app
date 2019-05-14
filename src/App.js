7c2c8f5f71c5bca2712b28a0a6e27a6c0c028297
sunnyruan83 <sunnyruan83@gmail.com>
2019-05-14 10:09:56 +0100

    finalfinal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import Header from './Header.js';
-import Footer from './Footer.js';
-import Search from './Search.js';
+import React from "react";
+import Header from "./Header.js";
+import Footer from "./Footer.js";
+import Search from "./Search.js";
 
-
-
-import './App.css';
-
-
+import "./App.css";
 
 function App() {
-    return (
-      <div>
-        <Header />
-        <Search />
-        <br></br>
-        <Footer />
-      </div>
-      
-    );
-  }
-
+  return (
+    <div>
+      <Header />
+      <br />
+      <Search />
+      <br />
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
